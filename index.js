1f7cf632960bc3251d572868b46242b78397e882
prasanthST <casinoprasanth@gmail.com>
2024-07-15 23:32:38 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+
+const app =express() //  express its a framework 
+app.use(cors()) // domain => eg: flipkart
+const port = 4000
+
+app.get("/first",(req,res)=>{
+    res.status(200).json({message:"Hello from first API"})
+})
+
+
+app.listen(port ,()=>{
+    console.log("Application is working on a port" , port);
+
+})
